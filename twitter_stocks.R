983e326c8a86002bf34faf02bd940213e67cb9a6
alex-server <alexander.svanevik@gmail.com>
2012-09-23 13:45:09 +0200

    Cleaned up twitter_stocks.R

--- twitter_stocks.R
+++ twitter_stocks.R
@@ -1,49 +1,27 @@
 library(twitteR)
 library(ggplot2)
+library(lubridate)
+library(stringr)
 
-#companies <- c('seadrill','algeta','funcom','diagenic','subsea 7','bionor')
-companies <- c('netconnect')
+# companies (keywords) to search for tweets for
+companies <- c('seadrill','algeta','funcom','diagenic','subsea 7','bionor','netconnect')
 
+# generate timestamp
+timestamp <- sprintf('%04d%02d%02d',year(now()),month(now()),day(now()))
+
+# fetch out_path from command line argument
+args <- commandArgs(TRUE)
+out_path <- args[1]
+end_str <- str_length(out_path)
+# ... and append '/' if out_path doesn't already end with one
+if (substr(out_path,end_str,end_str)!='/') {
+  out_path <- paste(out_path,'/',sep='')
+}
+
+# loop through companies and fetch tweets
 for (c in companies) {
   tweets <- searchTwitter(c,n=3000)
   df <- twListToDF(tweets)
-  write.table(df,file=paste(c,"20120806.csv",sep=""),sep=";")
+  write.table(df,file=paste(out_path,timestamp,"_",str_replace(c,' ','_'),".csv",sep=""),sep=";")
 }
 
-#pos.words <- scan('positive_words.txt',what='character',comment.char=';')
-#neg.words <- scan('negative_words.txt',what='character',comment.char=';')
-
-score.sentiment <- function(sentences,pos.words,neg.words,.progress='none')
-    {	require(plyr)	
-			require(stringr)		
-      #wegotavectorofsentences.plyrwillhandlealistoravectorasan"l"forus	
-      #wewantasimplearrayofscoresback,soweuse"l"+"a"+"ply"=laply:	
-      scores=laply(sentences,function(sentence,pos.words,neg.words){				
-      #cleanupsentenceswithR'sregex-drivenglobalsubstitute,gsub():		
-      sentence=gsub('[[:punct:]]','',sentence)		
-      sentence=gsub('[[:cntrl:]]','',sentence)		
-      sentence=gsub('\\d+','',sentence)		
-      #andconverttolowercase:		
-      sentence=tolower(sentence)		
-      #splitintowords.str_splitisinthestringrpackage		
-      word.list=str_split(sentence,'\\s+')		
-      #sometimesalist()isonelevelofhierarchytoomuch		
-      words=unlist(word.list)		
-      #compareourwordstothedictionariesofpositive&negativeterms		
-      pos.matches=match(words,pos.words)		
-      neg.matches=match(words,neg.words)			
-      #match()returnsthepositionofthematchedtermorNA		
-      #wejustwantaTRUE/FALSE:		
-      pos.matches=!is.na(pos.matches)		
-      neg.matches=!is.na(neg.matches)		
-      #andconvenientlyenough,TRUE/FALSEwillbetreatedas1/0bysum():		
-      score=sum(pos.matches)-sum(neg.matches)		
-      return(score)	
-      },pos.words,neg.words,
-      .progress=.progress)	
-      scores.df=data.frame(score=scores,text=sentences)	
-      return(scores.df)
-}
-
-#s <- score.sentiment(df$text,pos.words,neg.words)
-#qplot(s$score)
